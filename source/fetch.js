be2337a6a9d8e2557100d7245c59cb4e315a3d63
paulAlexSerban <paul.alex.serban@gmail.com>
2024-02-27 09:31:33 +0200

    updates

--- source/fetch.js
+++ source/fetch.js
@@ -2,16 +2,16 @@ const path = require('path');
 const fs = require('fs');
 const Parser = require('rss-parser');
 const utils = require('./utils');
-const { createFile } = utils;
+// const { createFile } = utils;
 const parser = new Parser();
 
-const source = process.argv[2];
+// const source = process.argv[2];
 
-const sourcesPath = path.resolve(__dirname, `sources`, `${source}.json`);
-const cachePath = path.resolve(__dirname, '..', 'cache', `${source}-cache.json`);
+// const sourcesPath = path.resolve(__dirname, `sources`, `${source}.json`);
+// const cachePath = path.resolve(__dirname, '..', 'cache', `${source}-cache.json`);
 
-const sources = JSON.parse(fs.readFileSync(sourcesPath));
-const cache = JSON.parse(fs.readFileSync(cachePath));
+// const sources = JSON.parse(fs.readFileSync(sourcesPath));
+// const cache = JSON.parse(fs.readFileSync(cachePath));
 
 const fetchRSS = async (url) => {
     try {
@@ -23,47 +23,46 @@ const fetchRSS = async (url) => {
 };
 
 const sortItems = (feed) => {
-    Object.keys(feed).forEach((key) => {
-        const { items } = feed[key];
+    const derivedFeed = { ...feed };
+    const { items } = derivedFeed;
+    items.forEach((key) => {
         const itemsWithDate = items.filter((item) => item.lastBuildDate || item.pubDate || item.isoDate);
-        feed[key].items = itemsWithDate.sort((a, b) => {
+        derivedFeed.items = itemsWithDate.sort((a, b) => {
             const aDate = new Date(a.lastBuildDate || a.pubDate || a.isoDate);
             const bDate = new Date(b.lastBuildDate || b.pubDate || b.isoDate);
             return bDate - aDate;
         });
     });
 
-    return feed;
+    return derivedFeed;
 };
 
 const keepOnlyLastWeekItems = (feed) => {
     const week = 7 * 24 * 60 * 60 * 1000;
     const now = new Date();
     const lastWeek = new Date(now.getTime() - week);
-
-    Object.keys(feed).forEach((key) => {
-        const { items } = feed[key];
-        const prevState = feed[key].items;
-        const itemsWithDate = items.filter((item) => item.lastBuildDate || item.pubDate || item.isoDate);
-        feed[key].items = itemsWithDate.filter((item) => {
-            const itemDate = new Date(item.lastBuildDate || item.pubDate || item.isoDate);
-            return itemDate > lastWeek;
-        });
-        if (feed[key].items.length < 5) {
-            feed[key].items = prevState.slice(0, 5);
-        }
-
-        if (feed[key].items.length > 10) {
-            feed[key].items = feed[key].items.slice(0, 10);
-        }
+    const derivedFeed = { ...feed };
+    const prevItemsState = [...derivedFeed.items];
+    derivedFeed.items = derivedFeed.items.filter((item) => item.lastBuildDate || item.pubDate || item.isoDate);
+    derivedFeed.items = derivedFeed.items.filter((item) => {
+        const itemDate = new Date(item.lastBuildDate || item.pubDate || item.isoDate);
+        return itemDate > lastWeek;
     });
+    if (derivedFeed.items.length < 5) {
+        derivedFeed.items = prevItemsState.slice(0, 5);
+    }
 
-    return feed;
+    if (derivedFeed.items.length > 10) {
+        derivedFeed.items = derivedFeed.items.slice(0, 10);
+    }
+
+    return derivedFeed;
 };
 
 const cleanItemsContent = (feed) => {
-    Object.keys(feed).forEach((key) => {
-        const { items } = feed[key];
+    const derivedFeed = { ...feed };
+    const { items } = derivedFeed;
+    items.forEach((item) => {
         feed[key].items = items.map((item) => {
             return {
                 ...item,
@@ -76,37 +75,66 @@ const cleanItemsContent = (feed) => {
         });
     });
 
-    return feed;
+    return derivedFeed;
+};
+
+const getSources = () => {
+    const sourcePath = path.resolve(__dirname, `sources`, `v2.json`);
+    const sourcesJSON = fs.readFileSync(sourcePath);
+    const sources = JSON.parse(sourcesJSON);
+    return sources;
 };
 
 const init = async () => {
     const promises = [];
-    const { sections } = sources;
-    sections.forEach((section) => {
-        const { items } = section;
-        items.forEach((item) => {
-            promises.push(
-                fetchRSS(item.url).then((feed) => {
-                    console.log(`[ info ] Fetched ${item.title}`);
-                    console.log(`[ info ] ${feed.title} has ${feed.items.length} items`);
-                    return { [item.title]: feed };
-                })
-            );
-        });
+    const sources = getSources();
+    sources.forEach((source) => {
+        promises.push(
+            fetchRSS(source.feed_url).then((feed) => {
+                console.log(`[ info ] Fetched ${source.title}`);
+                console.log(`[ info ] ${source.title} has ${feed?.items?.length} items`);
+                const date = new Date();
+                return {
+                    ...source,
+                    feed,
+                    date,
+                };
+            })
+        );
     });
 
-    Promise.all(promises).then((feeds) => {
-        feeds.forEach((feed) => {
-            cache.type = sections[0].title;
-            cache.feed = {
-                ...cache.feed,
-                ...cleanItemsContent(keepOnlyLastWeekItems(sortItems(feed))),
+    const feedCache = await Promise.all(promises).then((feeds) => {
+        const feedCache = feeds.map((rawFeed) => {
+            const feed = rawFeed.feed;
+            const sortedItems = sortItems(feed);
+            const lastWeekItems = keepOnlyLastWeekItems(sortedItems);
+            const cleanContent = cleanItemsContent(lastWeekItems);
+            return {
+                ...rawFeed,
+                feed: {
+                    items: [...lastWeekItems.items],
+                },
             };
-
-            const cacheString = JSON.stringify(cache, null, 2);
-            createFile(cachePath, cacheString);
         });
+        return feedCache;
     });
+    const feedCacheString = JSON.stringify(feedCache, null, 2);
+    const feedCachePath = path.resolve(__dirname, '..', 'cache', `feed-cache.json`);
+    fs.writeFileSync(feedCachePath, feedCacheString);
+
+    // Promise.all(promises).then((feeds) => {
+    //     feeds.forEach((feed) => {
+    //         console.log(`[ info ] Generating ${feed.title}`);
+    //         cache.type = sections[0].title;
+    //         cache.feed = {
+    //             ...cache.feed,
+    //             ...cleanItemsContent(keepOnlyLastWeekItems(sortItems(feed))),
+    //         };
+
+    //         const cacheString = JSON.stringify(cache, null, 2);
+    //         createFile(cachePath, cacheString);
+    //     });
+    // });
 };
 
 init();
